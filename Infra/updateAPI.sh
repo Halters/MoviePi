c5d7c1ddf118563cb9394150af48fd338fcca62e
Théo Armengou <theo.armengou@epitech.eu>
2019-07-04 21:25:58 +0200

    MOD
    Change owner of files after copying

--- Infra/updateAPI.sh
+++ Infra/updateAPI.sh
@@ -10,4 +10,4 @@ cp ../API/moviepiapi/* /var/www/FLASKAPPS/moviepiapi/
 source /var/www/FLASKAPPS/moviepiapi/bin/activate
 pip install -r /var/www/FLASKAPPS/moviepiapi/requirements.txt
 deactivate
-
+sudo chown -R www-data:www-data /var/www/FLASKAPPS
