94c752f7168608e6d60d8927b9897bec45cbcff4
Théo Armengou <theo.armengou@epitech.eu>
2019-07-05 03:21:51 +0200

    MOD
    Films Page verified if user is adult or no

--- API/moviepiapi/FilmsPage.py
+++ API/moviepiapi/FilmsPage.py
@@ -6,16 +6,27 @@
 ##
 
 from flask_restful import Resource
-from moviepiapi.utils import fill_return_packet, db
+from moviepiapi.utils import fill_return_packet, db, userH, check_auth_token
 from flask import request
 
 
 class FilmsPage(Resource):
     def get(self, page):
+        uuid = check_auth_token(request)
+        if not uuid:
+            return fill_return_packet(0, "Token Invalide", None)
+        uuid_binary = userH.getUUIDBinaryFromStr(uuid)
+        user = userH.getUserInformations(user_uuid=uuid_binary)
+        if not user:
+            return fill_return_packet(0, "Ce compte n'existe pas", None)
         numberPage = 15
         start = int(page) * numberPage
-        result = db.request(
-            "SELECT * from films LIMIT %s, %s", start, numberPage)
+        if user["age"] < 18:
+            result = db.request(
+                "SELECT id, title, release_date, image from films LIMIT %s, %s WHERE adult = 1", start, numberPage)
+        else:
+            result = db.request(
+                "SELECT id, title, release_date, image from films LIMIT %s, %s", start, numberPage)
         if not result:
             return fill_return_packet(0, "Pas de film trouvé a l'id demandé", None)
         return result
