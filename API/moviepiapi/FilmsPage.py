8c9c9ea01a35c739b69aa87d1e50bb7de5b55edb
Théo Armengou <theo.armengou@epitech.eu>
2019-07-05 03:16:29 +0200

    MOD
    Repushed films page

--- API/moviepiapi/FilmsPage.py
+++ API/moviepiapi/FilmsPage.py
@@ -11,8 +11,11 @@ from flask import request
 
 
 class FilmsPage(Resource):
-    def get(self, film_id):
-        result = None
+    def get(self, page):
+        numberPage = 15
+        start = int(page) * numberPage
+        result = db.request(
+            "SELECT * from films LIMIT %s, %s", start, numberPage)
         if not result:
             return fill_return_packet(0, "Pas de film trouvé a l'id demandé", None)
         return result
